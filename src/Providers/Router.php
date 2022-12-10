bac3276582a648a6a68ccaeee3e9e67de1489f28
= <nurgaleev.rt@gmail.com>
2022-12-10 17:05:27 +0300

    moved route list to constructor

--- src/Providers/Router.php
+++ src/Providers/Router.php
@@ -1,21 +1,27 @@
 <?php
 namespace provodd\base_framework\Providers;
 
-class Router{
-    public function start(){
+class Router
+{
+    public $route_list;
+
+    public function __construct($r)
+    {
+        $this->route_list = $r;
+    }
+
+    public function start()
+    {
         $route = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-        $route_list = [
-            "/" => ['controller' => 'Main', 'action' => 'index'],
-            '/index' => ['controller' => 'Main', 'action' => 'index'],
-        ];
+        $route_list = $this->route_list;
 
-        if (isset($route_list[$route])){
-            $controller = 'App\\Controllers\\'.$route_list[$route]['controller'];
+        if (isset($route_list[$route])) {
+            $controller = 'App\\Controllers\\' . $route_list[$route]['controller'];
             $object = new $controller();
             $action = $route_list[$route]['action'];
             $object->$action();
-        }else{
+        } else {
             echo 'Несуществующая страница';
         }
     }
